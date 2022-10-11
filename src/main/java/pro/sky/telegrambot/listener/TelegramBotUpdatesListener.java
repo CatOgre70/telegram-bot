4af45f2e7f85e796eb8b6ce28a5a587f030585fa
CatOgre70 <vasily.demin@gmail.com>
2022-10-11 03:12:49 +0300

    Course Work 1.1 - 1.4 (the dumbest bot ever)

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,9 +3,10 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -14,10 +15,13 @@ import java.util.List;
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
-    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
-    @Autowired
-    private TelegramBot telegramBot;
+    private final TelegramBot telegramBot;
+
+    public TelegramBotUpdatesListener(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
 
     @PostConstruct
     public void init() {
@@ -29,6 +33,13 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
             // Process your updates here
+            if(update.message().text().equalsIgnoreCase("/start")) {
+                SendMessage message = new SendMessage(update.message().chat().id(), "Привет! Это самый тупой бот на свете! Автор - Василий Демин");
+                SendResponse response = telegramBot.execute(message);
+                if(!response.isOk()){
+                    logger.error("Response error: {} {}", response.errorCode(), response.message());
+                }
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
